aab371adb607f1d40690616ebd8d09f1d769f830
Alexis Fetet <fetet.alexis@gmail.com>
2022-10-29 17:04:03 +0200

    adding main.py in checking

--- pre_commit_check.sh
+++ pre_commit_check.sh
@@ -5,6 +5,6 @@ conda.bat activate rte
 echo "running isort" 
 isort ./*
 echo "running autopep8" 
-autopep8 --in-place --global-config=.flake8 --max-line-length=79 -a -a -r ./py_france_rte/*
+autopep8 --in-place --global-config=.flake8 --max-line-length=79 -a -a -r ./py_france_rte/* main.py
 echo "running pylint" 
-pylint ./py_france_rte --disable=too-many-arguments --disable=too-few-public-methods -s=no
+pylint ./py_france_rte main.py --disable=too-many-arguments --disable=too-few-public-methods -s=no
